0612c0cf1a3bffa28c352fa77993867282e06ca5
Silambarasan R <silambarsan.rs@gmail.com>
2024-12-16 13:15:50 +0530

    Updated

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const headerRight = document.querySelector('.header_right');
     headerRight.classList.toggle('active');
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
